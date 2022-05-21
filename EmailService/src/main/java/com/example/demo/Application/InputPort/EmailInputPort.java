7164b464ee4926fafddaa377048eb8a87619702f
rigeita.telaku <rigeita.telaku@solaborate.com>
2022-05-22 01:38:56 +0200

    Revert "Add Register, Reservation and Email services"
    This reverts commit 29d38116deb73ec8713a3794274130059f9550a0.

--- EmailService/src/main/java/com/example/demo/Application/InputPort/EmailInputPort.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.Application.InputPort;
-
-public interface EmailInputPort {
-    void sendMessage(String to, String subject, String text);
-}
--- ReservationsService/src/main/java/com/example/demo/Core/OutputPort/TouristOutputPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Core.OutputPort;
-
-import com.example.demo.Core.Helper.TouristHelper;
-
-public interface TouristOutputPort {
-
-    TouristHelper getTouristByUsername(String username);
-
-}
